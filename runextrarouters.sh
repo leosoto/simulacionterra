93354951715005c54bb8993b15fd28b23ef63121
Pairing Workstation Bender <dev+bender@hashrocket.com>
2010-05-20 18:56:20 -0400

    Script para ejecutar routers adicionales

---
+++ runextrarouters.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$AMQPSERVER" == "" ] ; then
+    AMQPSERVER=localhost
+fi
+QUEUE=router.req
+echo "Iniciando 10 enrutadores con tiempo de procesamiento de 0.1 segundos"
+for i in {11..20}; do 
+   ./msgconsumer.rb \
+       -h $AMQPSERVER -q $QUEUE -p 0.1 \
+       --route -d "router.resp.canal1 router.resp.canal2 router.resp.canal3" \
+       > log-router-$i.txt &
+done
+
